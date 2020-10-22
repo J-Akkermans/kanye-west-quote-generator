80685f28d7dee46b75c6361b7653cef71b985eda
J Akkermans <99044668@mydavinci.nl>
2020-10-22 19:46:35 +0200

    tweak

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function getQuote(){
     fetch('https://api.kanye.rest')
     .then((response) => response.json())
     .then(function (quote) {
-        document.getElementById("quote").innerHTML = quote.quote;
+        document.getElementById("quote").innerHTML = " ' " + quote.quote + " ' -Kayne West ";
     })
 }
 getQuote()
